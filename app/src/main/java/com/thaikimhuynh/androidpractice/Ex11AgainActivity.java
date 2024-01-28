1a5dd34cf3aba0e2dabe82508d9003ee575422b5
kimhuynh <thaikimhuynh66@gmail.com>
2024-01-28 14:20:54 +0700

    Finished ex11

---
+++ app/src/main/java/com/thaikimhuynh/androidpractice/Ex11AgainActivity.java
@@ -0,0 +1,14 @@
+package com.thaikimhuynh.androidpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex11AgainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex11_again);
+    }
+}
